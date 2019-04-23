5c816208a06dc6a87e8cbd52d8196accd7d143b7
Vladislav Morozov <moroffv@gmail.com>
2019-04-23 23:02:02 +0400

    Add project setup

---
+++ uncomplicated-todo/uncomplicated-todo/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  uncomplicated-todo
+//
+//  Created by Vlad on 23/04/2019.
+//  Copyright © 2019 misshapes. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
