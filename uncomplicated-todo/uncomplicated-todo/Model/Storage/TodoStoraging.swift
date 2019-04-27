9c3243b67f1350bb8a17cbdef98ae566310cbc96
Vladislav Morozov <moroffv@gmail.com>
2019-04-27 21:53:33 +0400

    Add basic storage architecture

---
+++ uncomplicated-todo/uncomplicated-todo/Model/Storage/TodoStoraging.swift
@@ -0,0 +1,13 @@
+//
+//  TodoStoraging.swift
+//  uncomplicated-todo
+//
+//  Created by Vladislav Morozov on 27/04/2019.
+//  Copyright © 2019 misshapes. All rights reserved.
+//
+
+import Foundation
+
+protocol TodoStoraging: Storaging {
+    func edit()
+}
