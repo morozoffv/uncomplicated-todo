12b9cac3a6cc9203e0e210637edc313cbf5ea271
Vladislav Morozov <moroffv@gmail.com>
2019-05-03 14:42:36 +0400

    Add basic layout

---
+++ uncomplicated-todo/uncomplicated-todo/View/TodoCell.swift
@@ -0,0 +1,43 @@
+//
+//  TodoCell.swift
+//  uncomplicated-todo
+//
+//  Created by Vladislav Morozov on 03/05/2019.
+//  Copyright © 2019 misshapes. All rights reserved.
+//
+
+import UIKit
+
+class TodoCell: UITableViewCell {
+    
+    private let label = UILabel()
+    
+    override init(style: UITableViewCell.CellStyle, reuseIdentifier: String?) {
+        super.init(style: style, reuseIdentifier: reuseIdentifier)
+        customInit()
+    }
+    
+    required init?(coder aDecoder: NSCoder) {
+        super.init(coder: aDecoder)
+        customInit()
+    }
+    
+    private func customInit() {
+        translatesAutoresizingMaskIntoConstraints = false
+        label.translatesAutoresizingMaskIntoConstraints = false
+        addSubview(label)
+        
+        NSLayoutConstraint.activate([label.leadingAnchor.constraint(equalTo: self.leadingAnchor, constant: 20),
+                                     label.topAnchor.constraint(equalTo: self.topAnchor, constant: 10),
+                                     label.heightAnchor.constraint(equalToConstant: 20)])
+        
+//        label.leadingAnchor.constraint(equalTo: self.leadingAnchor, constant: 20).isActive = true
+//        label.topAnchor.constraint(equalTo: self.topAnchor, constant: 10).isActive = true
+//        label.heightAnchor.constraint(equalToConstant: 20).isActive = true
+    }
+    
+    func configure(todo: Todo) {
+        label.text = todo.name
+    }
+    
+}
--- uncomplicated-todo/uncomplicated-todo/ViewController.swift
+++ uncomplicated-todo/uncomplicated-todo/ViewController.swift
@@ -11,17 +11,60 @@ import UIKit
 class ViewController: UIViewController {
     
     private let networkManager = NetworkManager(network: Network())
-
+    
+    private var todos: [Todo] = [Todo(id: UUID(),
+                                      name: "Wash my hands",
+                                      priority: .highest,
+                                      dueDate: Date().addingTimeInterval(222222222),
+                                      creationDate: Date(),
+                                      completedDate: Date())]
+    
+    private lazy var tableView: UITableView = {
+        $0.translatesAutoresizingMaskIntoConstraints = false
+        $0.delegate = self
+        return $0
+    }(UITableView())
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        networkManager.requestTodos { result in
-            switch result {
-            case .success(let todos):
-                print(todos)
-            case .failure(let error):
-                print(error)
-            }
-        }
+        view.addSubview(tableView)
+        
+        NSLayoutConstraint.activate([
+            tableView.leadingAnchor.constraint(equalTo: view.leadingAnchor),
+            tableView.trailingAnchor.constraint(equalTo: view.trailingAnchor),
+            tableView.topAnchor.constraint(equalTo: view.topAnchor),
+            tableView.bottomAnchor.constraint(equalTo: view.bottomAnchor)
+            ])
+
+        tableView.register(TodoCell.self, forCellReuseIdentifier: TodoCell.self.description())
+        navigationItem.rightBarButtonItem = UIBarButtonItem(barButtonSystemItem: .add, target: self, action: #selector(addTodo))
+    }
+    
+    @objc func addTodo() {
+        todos.append(Todo(
+            id: UUID(),
+            name: "Another Todo #\(todos.count)",
+            priority: .lowest,
+            dueDate: Date(),
+            creationDate: Date(),
+            completedDate: Date())
+        )
     }
 }
 
+extension ViewController: UITableViewDelegate {
+}
+
+extension ViewController: UITableViewDataSource {
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return todos.count
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: TodoCell.self.description(), for: indexPath) as! TodoCell
+        cell.configure(todo: todos[indexPath.count])
+        return cell
+    }
+    
+    
+}
