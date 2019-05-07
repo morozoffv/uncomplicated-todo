3a9f3cf03c6168d52458940982992f9f079ee894
Vladislav Morozov <moroffv@gmail.com>
2019-05-07 09:35:46 +0400

    Add Dynamic for binding

---
+++ uncomplicated-todo/uncomplicated-todo/UIUtils/Dynamic.swift
@@ -0,0 +1,33 @@
+//
+//  Dynamic.swift
+//  uncomplicated-todo
+//
+//  Created by Vladislav Morozov on 07/05/2019.
+//  Copyright © 2019 misshapes. All rights reserved.
+//
+
+import Foundation
+
+class Dynamic<T> {
+    typealias Listener = (T) -> Void
+    var listener: Listener?
+    
+    func bind(_ listener: Listener?) {
+        self.listener = listener
+    }
+    
+    func bindAndFire(_ listener: Listener?) {
+        self.listener = listener
+        listener?(value)
+    }
+    
+    var value: T {
+        didSet {
+            listener?(value)
+        }
+    }
+    
+    init(_ v: T) {
+        value = v
+    }
+}
